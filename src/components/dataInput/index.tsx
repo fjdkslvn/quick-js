e43bea3eaef4e83124ddc8ccace4cdd2ab74d841
HyeJi <dhtnsehf9@gmail.com>
2024-04-17 14:16:27 +0900

    fix : Type error

--- src/components/dataInput/index.tsx
+++ src/components/dataInput/index.tsx
@@ -1,11 +1,11 @@
-'use client'
-
-import { useRecoilState } from 'recoil';
+import { useRecoilState, RecoilState } from 'recoil';
 import { dataSelector } from '@/recoil/atom';
 
-const DataInput: React.FC<{ dataType:string}> = ({dataType}) => {
-  const selectedData = dataSelector[dataType as keyof typeof dataSelector]; 
-  const [data, setData] = useRecoilState(selectedData);
+type DataSelectorType = keyof typeof dataSelector;
+
+const DataInput: React.FC<{ dataType: DataSelectorType }> = ({ dataType }) => {
+  const selectedData = dataSelector[dataType] as RecoilState<string>; // selectedData의 타입을 RecoilState<string>으로 명시
+  const [data, setData] = useRecoilState<string>(selectedData); // useRecoilState의 제네릭 타입을 string으로 명시
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setData(e.target.value);
@@ -13,7 +13,7 @@ const DataInput: React.FC<{ dataType:string}> = ({dataType}) => {
 
   return (
     <>
-      <textarea className="w-full min-h-24 px-3 py-2 text-sm" value={data} maxLength={1000} placeholder={'데이터를 넣어주세요.'} onChange={handleChange}/>
+      <textarea className="w-full min-h-24 px-3 py-2 text-sm" value={data} maxLength={1000} placeholder={'데이터를 넣어주세요.'} onChange={handleChange} />
     </>
   );
 };
--- src/components/functionBlock/codeBlock/index.tsx
+++ src/components/functionBlock/codeBlock/index.tsx
@@ -1,5 +1,5 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, RecoilState } from 'recoil';
 import { dataSelector } from '@/recoil/atom';
 import { functionData } from '@/constants/fucntionData';
 import { useEffect, useState } from 'react';
@@ -7,18 +7,18 @@ import styles from './styles.module.css';
 import { Copy } from '@public/svgs';
 import Toast from '@/components/functionBlock/toast';
 
-const CodeBlock: React.FC<{ dataType:string, id:number, displayCode: string; setResult: (resultData:string) => void }> = ({dataType, id, displayCode, setResult}) => {
+const CodeBlock: React.FC<{ dataType: string, id: number, displayCode: string; setResult: (resultData: string) => void }> = ({ dataType, id, displayCode, setResult }) => {
 
-  const selectedData = dataSelector[dataType as keyof typeof dataSelector]; 
-  const [data, setData] = useRecoilState(selectedData);
+  const selectedData = dataSelector[dataType as keyof typeof dataSelector] as RecoilState<string>; // 타입 명시
+  const [data, setData] = useRecoilState<string>(selectedData); // 제네릭 타입 명시
   const [showToast, setShowToast] = useState(false);
 
   useEffect(() => {
     createResult(); // 기본 결과값 채우기
-  }, [id])
+  }, [id]);
 
   const createResult = () => {
-    if(data && id){
+    if (data && id) {
       const useData = dataType === 'string' ? data : eval('(' + data + ')');
       const resultData = functionData[`func${id}`](useData);
       setResult(JSON.stringify(resultData));
@@ -30,12 +30,12 @@ const CodeBlock: React.FC<{ dataType:string, id:number, displayCode: string; set
     console.log(result);`;
 
     navigator.clipboard.writeText(code)
-    .then(() => {
-      setShowToast(true);
-    })
-    // .catch(err => {
-    //     console.error('복사 실패:', err);
-    // });
+      .then(() => {
+        setShowToast(true);
+      })
+      // .catch(err => {
+      //     console.error('복사 실패:', err);
+      // });
   }
 
   return (
@@ -43,7 +43,7 @@ const CodeBlock: React.FC<{ dataType:string, id:number, displayCode: string; set
       <div className="w-full">
         <div className="bg-blue-100 w-full h-9 rounded-t-lg px-4 py-2 text-sm text-gray-500 font-medium flex justify-between">
           <div>index.js</div>
-          <Copy className={["cursor-pointer", styles.copyImage].join(' ')} onClick={codeCopy}/>
+          <Copy className={["cursor-pointer", styles.copyImage].join(' ')} onClick={codeCopy} />
         </div>
         <div className={["bg-blue-50 w-full min-h-24 rounded-b-lg px-4 py-2 text-sm text-gray-800 font-medium", styles.codeHighlight].join(' ')}>
           <SyntaxHighlighter language={"javascript"}>
@@ -51,7 +51,7 @@ const CodeBlock: React.FC<{ dataType:string, id:number, displayCode: string; set
           </SyntaxHighlighter>
           <button className="block mx-auto mb-5 bg-blue-300 px-10 h-10 rounded-lg text-gray-700 font-medium hover:bg-blue-200" onClick={createResult}>실행</button>
         </div>
-        {showToast && <Toast message={'복사되었습니다.'} onClose={() => setShowToast(false)}/>}
+        {showToast && <Toast message={'복사되었습니다.'} onClose={() => setShowToast(false)} />}
       </div>
     </>
   );
