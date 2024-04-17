24b5448836b2d13aa4e6600b171b52d9b7fb7783
HyeJi <dhtnsehf9@gmail.com>
2024-04-17 14:07:12 +0900

    feat : Modify workspace path

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
     <div className="border-b border-inherit grid place-items-center">
         <nav className="flex flex-row items-center max-w-screen-xl w-full h-16">
             <Link className="ml-6 mr-auto text-lg" href="/">QuickJS</Link>
-            <Link className="mr-6" href="/docs/1">작업실</Link>
+            <Link className="mr-6" href="/docs/string">작업실</Link>
             <Link className="mr-6" href="/notice">공지사항</Link>
         </nav>
     </div>
