c51dda18d1b310751b70f49763dcdcf9de47259d
HyeJi <dhtnsehf9@gmail.com>
2024-04-28 16:48:43 +0900

    fix : Notice Description Dummy data removal

--- src/app/(nav)/notice/page.tsx
+++ src/app/(nav)/notice/page.tsx
@@ -21,7 +21,6 @@ export default function Page() {
     <div className="flex justify-center w-full">
       <div className="w-full max-w-screen-md px-4 py-8">
         <h1 className="text-3xl font-bold mb-10">공지사항</h1>
-        <div>최신 업데이트 소식 및 </div>
         {noticeList?.map((notice)=>(
           <Notice key={notice.id} notice={notice}/>
         ))}
