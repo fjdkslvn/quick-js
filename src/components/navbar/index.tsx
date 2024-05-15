42a9f7e0d37fe74ef969871f35f4837e0ff0e94d
HyeJi <dhtnsehf9@gmail.com>
2024-05-16 00:38:09 +0900

    feat: Add functionality to collapse and expand the sidebar menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 import Link from "next/link";
 import { useEffect, useState } from 'react';
-import { usePathname } from 'next/navigation';
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import ThemeSeletor from "../themeSeletor";
@@ -11,15 +10,11 @@ import { SideMenu, sideMenuData } from '@/recoil/sideMenuAtom';
 const Navbar: React.FC<{ sideMenuList:SideMenu[] }> = ({ sideMenuList }) => {
   const [sideMenu, setSideMenu] = useRecoilState<SideMenu[]>(sideMenuData);
   const [toggle, setToggle] = useState(false);
-
-  const handleToggle = () => {
-    setToggle(!toggle);
-  }
-
+  
   useEffect(() => {
     setSideMenu(sideMenuList);
   },[]);
-
+  
   useEffect(() => {
     if (toggle) {
       document.body.style.overflow = 'hidden'; // 스크롤 막기
@@ -28,7 +23,9 @@ const Navbar: React.FC<{ sideMenuList:SideMenu[] }> = ({ sideMenuList }) => {
     }
   }, [toggle]);
 
-  const pathName = usePathname();
+  const handleToggle = () => {
+    setToggle(!toggle);
+  }
 
   return (
     <div className="border-b border-zinc-200 grid place-items-center sticky top-0 z-10 bg-white dark:bg-backDarkColor dark:border-zinc-700">
@@ -53,34 +50,6 @@ const Navbar: React.FC<{ sideMenuList:SideMenu[] }> = ({ sideMenuList }) => {
           <div>
             <Link className="text-sm text-gray-500 pl-2 h-9 my-1 flex items-center rounded hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-neutral-800" href="/notice" onClick={handleToggle}>공지사항</Link>
           </div>
-
-          {sideMenu?.map((menu) => (
-            <div key={`menu_${menu.id}`} className="text-sm">
-              <Link
-                href={menu.link}
-                className={pathName === menu.link
-                            ? "text-blue-500 font-bold pl-2 h-9 my-1 flex items-center rounded"
-                            : "text-gray-500 pl-2 h-9 my-1 flex items-center rounded hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-neutral-800"}
-                onClick={handleToggle}
-              >
-                {menu.name}
-              </Link>
-              {menu.side_submenu.map((sub_menu) => (
-                  <div className="flex flex-row w-full items-center" key={`sub_menu_${sub_menu.id}`}>
-                    <div className="w-px h-10 mx-3 bg-gray-300 dark:bg-gray-600"></div>
-                    <Link
-                      href={sub_menu.link}
-                      className={pathName === sub_menu.link
-                                  ? "w-full text-blue-500 font-bold pl-2 h-9 flex items-center rounded my-0.5"
-                                  : "w-full text-gray-500 pl-2 h-9 flex items-center rounded my-0.5 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-neutral-800"}
-                      onClick={handleToggle}
-                    >
-                      {sub_menu.name}
-                    </Link>
-                  </div>
-              ))}
-            </div>
-          ))}
         </div>}
     </div>
   );
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,36 +4,69 @@ import Link from "next/link";
 import { usePathname } from 'next/navigation';
 import { useRecoilState } from 'recoil';
 import { SideMenu, sideMenuData } from '@/recoil/sideMenuAtom';
+import { useEffect, useState } from "react";
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+interface MenuState {
+  [key: string]: boolean;
+}
 
 const Sidebar: React.FC = () => {
   const [sideMenu, setSideMenu] = useRecoilState<SideMenu[]>(sideMenuData);
+  const [menuState, setMenuState] = useState<MenuState>({});
   const pathName = usePathname();
+
+  // 대메뉴를 토글하는 함수
+  const toggleMenu = (id:number, link:string) => {
+    setMenuState({
+      ...menuState,
+      [`menu_${id}`]: pathName !== link ? true : !menuState[`menu_${id}`] // 특정 메뉴 아이디의 상태를 반전시킴
+    });
+  };
+  
+  useEffect(() => {
+    if(pathName.includes('/docs/')){
+      const target = pathName.split('/')[2];
+      const targetData = sideMenu.find(menu => menu.name === target);
+      setMenuState({
+        ...menuState,
+        [`menu_${targetData?.id}`] : true
+      });
+    }
+  },[sideMenu, pathName]);
 
   return (
     <nav className="min-w-56 px-6 py-4 h-max sticky top-16 hidden md:block">
       {sideMenu?.map((menu) => (
         <div key={`menu_${menu.id}`} className="text-sm">
           <Link
+          onClick={() => toggleMenu(menu.id, menu.link)}
             href={menu.link}
             className={pathName === menu.link
-                        ? "text-blue-500 font-bold pl-2 h-9 my-1 flex items-center rounded"
-                        : "text-gray-500 pl-2 h-9 my-1 flex items-center rounded hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-neutral-800"}
+                        ? "flex justify-between text-blue-500 font-bold pl-2 pr-1 h-9 my-1 flex items-center rounded"
+                        : "flex justify-between text-gray-500 pl-2 pr-1 h-9 my-1 flex items-center rounded hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-neutral-800"}
           >
             {menu.name}
+            {menuState[`menu_${menu.id}`]
+            ?<KeyboardArrowDownIcon/>
+            :<KeyboardArrowRightIcon/>}
           </Link>
-          {menu.side_submenu.map((sub_menu) => (
-              <div className="flex flex-row w-full items-center" key={`sub_menu_${sub_menu.id}`}>
-                <div className="w-px h-10 mx-3 bg-gray-300 dark:bg-gray-600"></div>
-                <Link
-                  href={sub_menu.link}
-                  className={pathName === sub_menu.link
-                              ? "w-full text-blue-500 font-bold pl-2 h-9 flex items-center rounded my-0.5"
-                              : "w-full text-gray-500 pl-2 h-9 flex items-center rounded my-0.5 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-neutral-800"}
-                >
-                  {sub_menu.name}
-                </Link>
-              </div>
-          ))}
+          <div className={`overflow-hidden transition-max-height transition-height duration-500 ${menuState[`menu_${menu.id}`] ? 'max-h-96' : 'max-h-0'}`}>
+            {menu.side_submenu.map((sub_menu) => (
+                <div className="flex flex-row w-full items-center" key={`sub_menu_${sub_menu.id}`}>
+                  <div className="w-px h-10 mx-3 bg-gray-300 dark:bg-gray-600"></div>
+                  <Link
+                    href={sub_menu.link}
+                    className={pathName === sub_menu.link
+                                ? "w-full text-blue-500 font-bold pl-2 h-9 flex items-center rounded my-0.5"
+                                : "w-full text-gray-500 pl-2 h-9 flex items-center rounded my-0.5 hover:bg-gray-100 hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-200 dark:hover:bg-neutral-800"}
+                  >
+                    {sub_menu.name}
+                  </Link>
+                </div>
+            ))}
+          </div>
         </div>
       ))}
     </nav>
