63cd3f39af866fe8ca8863f81a5ba785371bd9a3
HyeJi <dhtnsehf9@gmail.com>
2024-05-22 23:37:38 +0900

    feat : Add link to git issues page
    Link to a page where questions and improvements are requested.

--- src/components/scrollNav/index.tsx
+++ src/components/scrollNav/index.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import Link from "next/link";
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+
 const ScrollNav: React.FC<{scrollList:any[]}> = ({scrollList}) => {
 
     const handleClick = (id: number) => {
@@ -16,6 +19,9 @@ const ScrollNav: React.FC<{scrollList:any[]}> = ({scrollList}) => {
           <div key={`scrollTitle_${scrollInfo.id}`} className="text-sm font-semibold text-gray-600 pb-3 cursor-pointer dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-500" onClick={() => handleClick(scrollInfo.id)}>{scrollInfo.title}</div>
         ))}
         <div className="w-full h-px bg-zinc-200 my-4 dark:bg-zinc-700"></div>
+        <Link className="text-sm flex items-center text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100" target="_blank" href="https://github.com/fjdkslvn/quick-js/issues/new">
+          질문 및 피드백 제안 <QuestionAnswerIcon className="text-sm ml-2"/>
+        </Link>
       </div>
   );
 };
