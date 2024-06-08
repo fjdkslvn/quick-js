48de879ab5a981012bfbd0ae8c4124eed6eca24f
HyeJi <dhtnsehf9@gmail.com>
2024-06-08 13:06:35 +0900

    Remove footer component comment

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,18 +3,6 @@
 import Link from "next/link";
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-// const Footer: React.FC = () => {
-  
-//   return(
-//     <div className="border-solid border-t border-gray-300 p-4">
-//       <div className="w-full flex justify-center">
-//           <Link href="https://fjdkslvn.tistory.com/" target="_blank">티스토리</Link>
-//           <Link href="https://github.com/fjdkslvn/quick-js" target="_blank"><GitHubIcon/></Link>
-//       </div>
-//     </div>
-//   )
-// };
-
 const Footer: React.FC = () => {
   return (
     <footer className="w-full flex justify-center border-solid border-t border-zinc-200 dark:border-zinc-700 p-6 text-xs text-gray-600 dark:text-gray-300">
