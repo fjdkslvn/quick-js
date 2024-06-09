253712ff85ac625374c3bc8383cde65c4337f52a
HyeJi <dhtnsehf9@gmail.com>
2024-06-09 15:55:48 +0900

    feat: Sidebar closed when changing page (2)

--- src/components/dataInput/index.tsx
+++ src/components/dataInput/index.tsx
@@ -1,19 +1,36 @@
 import { useRecoilState, RecoilState } from 'recoil';
 import { dataSelector } from '@/recoil/funcDataAtom';
+import { useEffect, useRef } from 'react';
+import { usePathname } from 'next/navigation';
 
 type DataSelectorType = keyof typeof dataSelector;
 
 const DataInput: React.FC<{ dataType: DataSelectorType }> = ({ dataType }) => {
-  const selectedData = dataSelector[dataType] as RecoilState<string>; // selectedData의 타입을 RecoilState<string>으로 명시
-  const [data, setData] = useRecoilState<string>(selectedData); // useRecoilState의 제네릭 타입을 string으로 명시
+  const selectedData = dataSelector[dataType] as RecoilState<string>;
+  const [data, setData] = useRecoilState<string>(selectedData);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const pathName = usePathname();
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setData(e.target.value);
-  }
+  };
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.blur();
+    }
+  }, [pathName]);
 
   return (
     <>
-      <textarea className="w-full min-h-24 px-3 py-2 text-sm dark:bg-backDarkColor" value={data} maxLength={1000} placeholder={'데이터를 넣어주세요.'} onChange={handleChange} />
+      <textarea
+        ref={textareaRef}
+        className="w-full min-h-24 px-3 py-2 text-sm dark:bg-backDarkColor"
+        value={data}
+        maxLength={1000}
+        placeholder={'데이터를 넣어주세요.'}
+        onChange={handleChange}
+      />
     </>
   );
 };
