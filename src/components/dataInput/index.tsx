b749a7ff931f06159b1d4abde3e6938147fc1742
HyeJi <dhtnsehf9@gmail.com>
2024-06-09 16:35:09 +0900

    feat: Sidebar closed when changing page (7)

--- src/components/dataInput/index.tsx
+++ src/components/dataInput/index.tsx
@@ -7,29 +7,27 @@ type DataSelectorType = keyof typeof dataSelector;
 const DataInput: React.FC<{ dataType: DataSelectorType }> = ({ dataType }) => {
   const selectedData = dataSelector[dataType] as RecoilState<string>;
   const [data, setData] = useRecoilState<string>(selectedData);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const focusRef = useRef<HTMLDivElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setData(e.target.value);
   };
 
   useEffect(() => {
-    if (textareaRef.current) {
-      textareaRef.current.blur();
+    if (focusRef.current) {
+      focusRef.current.focus();
     }
   }, []);
 
   return (
     <>
+      <div ref={focusRef} autoFocus={true}></div>
       <textarea
-        ref={textareaRef}
         className="w-full min-h-24 px-3 py-2 text-sm dark:bg-backDarkColor"
         value={data}
         maxLength={1000}
         placeholder={'데이터를 넣어주세요.'}
         onChange={handleChange}
-        autoFocus={false}
-        tabIndex={-1}
       />
     </>
   );
