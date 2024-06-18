b5f493ec0241ea590197abaa43bacdd6708cb4b5
HyeJi <dhtnsehf9@gmail.com>
2024-06-18 23:33:38 +0900

    feat : Add data reset button

--- src/components/dataInput/index.tsx
+++ src/components/dataInput/index.tsx
@@ -1,17 +1,26 @@
-import { useRecoilState, RecoilState } from 'recoil';
+import { useRecoilState, RecoilState, useResetRecoilState } from 'recoil';
 import { dataSelector } from '@/recoil/funcDataAtom';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 type DataSelectorType = keyof typeof dataSelector;
 
 const DataInput: React.FC<{ dataType: DataSelectorType }> = ({ dataType }) => {
   const selectedData = dataSelector[dataType] as RecoilState<string>;
   const [data, setData] = useRecoilState<string>(selectedData);
+  const [resetActive, setResetActive] = useState(false);
   const focusRef = useRef<HTMLDivElement>(null);
+  const resetValue = useResetRecoilState(selectedData);
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setResetActive(true);
     setData(e.target.value);
   };
+
+  const resetData = () => {
+    setResetActive(false);
+    resetValue();
+  }
 
   useEffect(() => {
     // 페이지 로드시 textarea의 focus를 뻇는 작업
@@ -21,7 +30,7 @@ const DataInput: React.FC<{ dataType: DataSelectorType }> = ({ dataType }) => {
   }, []);
 
   return (
-    <>
+    <div className="relative">
       <div ref={focusRef} autoFocus={true}></div>
       <textarea
         className="w-full min-h-24 px-3 py-2 text-sm dark:bg-backDarkColor"
@@ -30,7 +39,8 @@ const DataInput: React.FC<{ dataType: DataSelectorType }> = ({ dataType }) => {
         placeholder={'데이터를 넣어주세요.'}
         onChange={handleChange}
       />
-    </>
+      <RestartAltIcon className={["cursor-pointer absolute right-2 bottom-4 text-gray-600 dark:text-gray-300", resetActive && "text-pink-600 dark:text-pink-600"].join(' ')} onClick={resetData}/>
+    </div>
   );
 };
 
